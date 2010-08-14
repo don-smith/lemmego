29012639ef2035f1fd80bd1eb67813b070e815a2
Don Smith <locksmithdon@gmail.com>
2010-08-14 03:09:29 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lemmego::Application.config.secret_token = '172d00dfbc4ae19e0ef983130dd72ffd5c341896f62b87f3c1401c6b7f94858448f1065ac45fe997babf4d4c18b1551719a5329449f5f26dce06dd5d33f7b781'
