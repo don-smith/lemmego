8a4357d395bfbfab26db0aff3982f8146593ef3b
Don Smith <locksmithdon@gmail.com>
2010-09-11 18:14:08 -0700

    cleaned seeds.rb (since I wasn't using it correctly) and fixed the order of the navigation menu items

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,37 +8,4 @@
 # 
 # To seed the test db, run: rake db:seed RAILS_ENV=test
 # 
-
-#
-# Users
-#
-pam = User.create(:email => 'pam@producer.com', 
-                  :password => 'seekret!', 
-                  :password_confirmation => 'seekret!')
-                  pam.save
-                  
-                  
-sam = User.create(:email => 'sam@speaker.com', 
-                  :password => 'seekret!', 
-                  :password_confirmation => 'seekret!')
-                  sam.save
-
-ari = User.create(:email => 'ari@attendee.com', 
-                  :password => 'seekret!', 
-                  :password_confirmation => 'seekret!')
-                  ari.save
-                  
-vic = User.create(:email => 'vic@visitor.com', 
-                  :password => 'seekret!', 
-                  :password_confirmation => 'seekret!')
-                  vic.save
-
-# 
-# Events
-# 
-devcon = Event.create(:name => 'DevCon Northwest',
-                      :description => "The best developer conference in the northwest",
-                      :start_time => Time.new + (60 * 60 * 24 * 30), # 30 days from now
-                      :end_time => Time.new + (60 * 60 * 24 * 33), # 3 day event
-                      :location => 'Seattle City Center')
-                      devcon.producers << pam.create_producer(:is_owner => true)
+ß
